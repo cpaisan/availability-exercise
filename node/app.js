02b480d4952278fde69fdf7c682477787d3dcd65
Charlie Paisan <cppaisan@gmail.com>
2019-07-30 18:57:03 -0700

    [feature] Update /today endpoint to include availability data from Thinkful API
    - Added two helper functions
      - fetchThinkfulAvailabilityData to request Thinkful API for availability
      - formatThinkfulData to format the data in a friendlier way to be displayed on the FE
    - Added test to check that the payload response is sent with the correct data format

--- node/app.js
+++ node/app.js
@@ -1,20 +1,68 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/today", (req, res) => {
-    res.send({
-        today: today()
+const today = () => new Date().toLocaleDateString();
+
+let thinkfulAvailabilityData = {};
+
+/**
+ * @async function that requests the Thinkful API for advisor availability
+ * @param {} - function does not accept any arguments
+ * @return {Object} - returns an object where the keys are days and values are
+ *                    objects with keys that represent date & time availability w/
+ *                    advisor ID's as values
+ * @throws {} - Thinkful API is unavailable and we display the error in the console
+ */
+const fetchThinkfulAvailabilityData = async () => {
+  try {
+    const thinkfulApiResponse = await fetch(
+      "https://www.thinkful.com/api/advisors/availability"
+    );
+    const data = await thinkfulApiResponse.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+  * @param {object} data - Thinkful availability API data
+  * @return {object} formattedData - availability data grouped by advisor
+                     each advisor key has a value of an array that contains objects
+                     w/ availability (date/time), whether the date/time is booked,
+                     and the student name of the booking
+*/
+const formatThinkfulData = data =>
+  Object.values(data).reduce((formattedData, availabilityObject) => {
+    Object.entries(availabilityObject).forEach(([dateTime, advisor]) => {
+      let advisorAvailabity = formattedData[advisor] || [];
+      advisorAvailabity = [
+        ...advisorAvailabity,
+        { availability: dateTime, isBooked: false, studentName: null }
+      ];
+      formattedData = {
+        ...formattedData,
+        [advisor]: advisorAvailabity
+      };
     });
+    return formattedData;
+  }, {});
+
+app.get("/today", async (req, res) => {
+  const data = await fetchThinkfulAvailabilityData();
+  const formattedThinkfulData = formatThinkfulData(data);
+  res.send({
+    today: today(),
+    availability: formattedThinkfulData
+  });
 });
 
-function today() {
-    return new Date().toLocaleDateString();
-}
 
 app.today = today;
 module.exports = app;
--- node/app.test.js
+++ node/app.test.js
@@ -1,7 +1,97 @@
+const request = require("supertest");
+const nock = require("nock");
 const app = require("./app");
 
+const thinkfulApiResponse = {
+  "2019-08-04": {
+    "2019-08-04T10:30:00-04:00": 319369,
+    "2019-08-04T11:00:00-04:00": 319369,
+    "2019-08-04T20:00:00-04:00": 319369
+  },
+  "2019-08-01": {
+    "2019-08-01T11:00:00-04:00": 372955,
+    "2019-08-01T19:00:00-04:00": 499660,
+    "2019-08-01T21:00:00-04:00": 399956,
+    "2019-08-01T02:00:00-04:00": 399956,
+    "2019-08-01T10:00:00-04:00": 372955,
+    "2019-08-01T12:00:00-04:00": 419054
+  }
+};
+
+const formattedThinkfulData = {
+  "319369": [
+    {
+      availability: "2019-08-04T10:30:00-04:00",
+      isBooked: false,
+      studentName: null
+    },
+    {
+      availability: "2019-08-04T11:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    },
+    {
+      availability: "2019-08-04T20:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    }
+  ],
+  "372955": [
+    {
+      availability: "2019-08-01T11:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    },
+    {
+      availability: "2019-08-01T10:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    }
+  ],
+  "399956": [
+    {
+      availability: "2019-08-01T21:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    },
+    {
+      availability: "2019-08-01T02:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    }
+  ],
+  "419054": [
+    {
+      availability: "2019-08-01T12:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    }
+  ],
+  "499660": [
+    {
+      availability: "2019-08-01T19:00:00-04:00",
+      isBooked: false,
+      studentName: null
+    }
+  ]
+};
+
 describe("today", () => {
-    it("returns today's formatted date", () => {
-        expect(app.today()).toBe(new Date().toLocaleDateString());
-    });
+  beforeEach(() => {
+    nock("https://www.thinkful.com")
+      .get("/api/advisors/availability")
+      .reply(200, {
+        ...thinkfulApiResponse
+      });
+  });
+
+  it("returns today's formatted date", () => {
+    expect(app.today()).toBe(new Date().toLocaleDateString());
+  });
+
+  it("returns API data formatted", async () => {
+    const { body: responseData } = await request(app).get("/today");
+    const { availability } = responseData;
+    expect(availability).toEqual(formattedThinkfulData);
+  });
 });
